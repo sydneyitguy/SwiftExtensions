76e7e4eb8d9d1e5db813aee7ba93d031545c0f66
Sebastian Kim <sydneyitguy@gmail.com>
2015-05-01 14:25:30 +0900

    Add regular expression on String
    missing

--- String.swift
+++ String.swift
@@ -1,4 +1,8 @@
 extension String {
+    var length: Int {
+        return count(self)
+    }
+
     func replace(target: String, withString: String) -> String {
         return self.stringByReplacingOccurrencesOfString(target, withString: withString, options: NSStringCompareOptions.LiteralSearch, range: nil)
     }
@@ -12,5 +16,30 @@ extension String {
         }
 
         return dropLast(join)
+    }
+}
+
+infix operator =~ {}
+//  Pattern matching using a regular expression
+public func =~ (string: String, pattern: String) -> Bool {
+    let regex = ExSwift.regex(pattern, ignoreCase: false)!
+    let matches = regex.numberOfMatchesInString(string, options: nil, range: NSMakeRange(0, string.length))
+
+    return matches > 0
+
+}
+
+public class ExSwift {
+    internal class func regex (pattern: String, ignoreCase: Bool = false) -> NSRegularExpression? {
+        var options = NSRegularExpressionOptions.DotMatchesLineSeparators.rawValue
+
+        if ignoreCase {
+            options = NSRegularExpressionOptions.CaseInsensitive.rawValue | options
+        }
+
+        var error: NSError? = nil
+        let regex = NSRegularExpression(pattern: pattern, options: NSRegularExpressionOptions(rawValue: options), error: &error)
+
+        return (error == nil) ? regex : nil
     }
 }
